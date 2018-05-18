4818e9c77d25a8edb69ce2a8542fb0ac1f8bfc64
Balz Guenat <balz.guenat@gmail.com>
2018-05-18 18:27:14 +0200

    bit of a refactor

--- ticlif.py
+++ ticlif.py
@@ -1,10 +1,17 @@
 import os
+import sys
 import time
 import msvcrt
 import shutil
-from collections import deque
+from enum import Enum, unique, auto
+from collections import deque, namedtuple
 
 CLEAR_CMD = 'cls'
+AUTO_REFRESH_INTERVAL = 0
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 class DroppingList:
@@ -21,14 +28,27 @@ class DroppingList:
         self.values.append(x)
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Box:
+    def __init__(self, value):
+        self.value = value
 
-    def __iter__(self):
-        return iter((self.x, self.y))
+    def __str__(self):
+        return str(self.value)
 
+    def __add__(self, other):
+        if isinstance(other, Box):
+            return self.value + other.value
+        else:
+            return self.value + other
+
+    def __iadd__(self, other):
+        self.value = self + other
+
+
+_Point = namedtuple('Point', ['x', 'y'])
+
+
+class Point(_Point):
     def __add__(self, other):
         if isinstance(other, Point):
             return Point(self.x + other.x, self.y + other.y)
@@ -52,13 +72,23 @@ class Point:
     def __str__(self):
         return '(' + str(self.x) + ',' + str(self.y) + ')'
 
+    #     def __init__(self, x, y):
+    #         self.x = x
+    #         self.y = y
+    #
+    #     def __iter__(self):
+    #         return iter((self.x, self.y))
 
-terminal_size = Point(*shutil.get_terminal_size()) + -2
 
 
-def flow_text(str: str, width: int) -> list:
+def get_window_size() -> Point:
+    w, h = shutil.get_terminal_size()
+    return Point(w, h - 2)
+
+
+def flow_text(text: str, width: int) -> list:
     result = []
-    for line in str.expandtabs(tabsize=2).splitlines():
+    for line in text.expandtabs(tabsize=2).splitlines():
         block_start = 0
         while block_start + width < len(line):
             result.append(line[block_start:block_start+width])
@@ -76,11 +106,10 @@ def draw(elem, state):
         state.properties['frame'] = 0
     state.properties['frame'] += 1
     clear()
-    (w, h) = terminal_size
+    (w, h) = get_window_size()
 
-    elem.resize(w, h)
     s = ""
-    for row in range(elem.cur_size.y):
+    for row in range(h):
         line = elem.get_content(row)
         if row == state.pos.y:
             line = line[:max(0, state.pos.x)] + "$" + line[state.pos.x + 1:]
@@ -101,9 +130,60 @@ def make_buf(state):
 
 
 class State:
-    pos = Point(0, 0)
-    debug = None
-    properties = {}
+    def __init__(self):
+        self.pos = Point(0, 0)
+        self.properties = {}
+
+
+class Controller:
+    def __init__(self):
+        self.state = State()
+        self.roots = []
+
+    def attach(self, root):
+        self.roots.append(root)
+        root.controller = self
+
+    def update(self):
+        for root in self.roots:
+            win_size = get_window_size()
+            root.resize(win_size)
+            root.update()
+
+    def process_user_input(self, user_input):
+        root = self.roots[0]
+        state = self.state
+        Debug.recent_inputs.append(user_input)
+
+        if user_input == b'\x03':
+            raise KeyboardInterrupt
+        if user_input in [b'\000', b'\xe0']:
+            pass
+
+        if user_input in [b'r', b'M']:
+            state.pos = Point(min(get_window_size().x - 1, state.pos.x + 1), state.pos.y)
+        elif user_input in [b'l', b'K']:
+            state.pos = Point(max(0, state.pos.x - 1), state.pos.y)
+        elif user_input in [b'd', b'P']:
+            state.pos = Point(state.pos.x, min(get_window_size().y - 1, state.pos.y + 1))
+        elif user_input in [b'u', b'H']:
+            state.pos = Point(state.pos.x, max(0, state.pos.y - 1))
+        elif user_input == b'\r':
+            event = Event()
+            event.pos = state.pos
+            event.key = user_input
+            root.action(event)
+        elif user_input == b'\x1b':
+            raise TerminationRequestedException
+        # else:
+        #     print("exiting. input was: " + str(user_input))
+        #     break
+
+    def move_cursor_to_next(self):
+        # TODO find out in which root the cursor is
+        for root in self.roots:
+            # root.element_at(*self.state.pos).next_element()
+            elem = root.element_at(*self.state.pos)
 
 
 class Element:
@@ -116,29 +196,72 @@ class Element:
         self.direction = 'vertical'
         self.separator_char = None
         self.separate = True
-        self.content = lambda self: None
+        self.fetch_content = lambda self: ""
+        self.content = None
+        self.parent = None
+        self.__controller = None
         self.children = []
         self.event_handler = None
+        self.id = None
 
-    def resize(self, w, h):
-        self.cur_size = Point(w, h)
+    def __str__(self):
+        return self.id or super(Element, self).__str__()
+
+    @property
+    def controller(self):
+        return self.__controller or self.parent.controller
+
+    @controller.setter
+    def controller(self, controller):
+        self.__controller = controller
+        for child in self.children:
+            child.controller = controller
+
+    def update(self):
+        self.content = self.fetch_content(self)
+        self.update_children()
+
+    def update_children(self):
+        for child in self.children:
+            child.update()
+
+    def next_element(self):
+        """
+        Returns the next element after this element.
+        This element may decide the traversal order.
+        :return: The next element after this or None if this is the last element.
+        """
+        return self.parent.next_child(self)
+
+    def next_child(self, child):
+        """Returns the child after the specified child"""
+        for i in range(len(self.children)):
+            if self.children[i] is child:
+                if i + 1 < len(self.children):
+                    return self.children[i + 1]
+                else:
+                    return self.parent.next_child(self) if self.parent else None
+        raise Exception("given child is not a child of this parent")
+
+    def resize(self, size: Point):
+        self.cur_size = size
         if len(self.children) > 0:
             if self.direction == 'vertical':
-                space_left = h
+                space_left = size.y
                 if self.separate:
                     space_left -= len(self.children) - 1
-                child_size = Point(w, space_left // len(self.children))
+                child_size = Point(size.x, space_left // len(self.children))
                 last_child_size = child_size + Point(0, space_left % len(self.children))
-                self.children[-1].resize(*last_child_size)
             else:
-                space_left = w
+                space_left = size.x
                 if self.separate:
                     space_left -= len(self.children) - 1
-                child_size = Point(space_left // len(self.children), h)
+                child_size = Point(space_left // len(self.children), size.y)
                 last_child_size = child_size + Point(space_left % len(self.children), 0)
-                self.children[-1].resize(*last_child_size)
+
             for child in self.children[:-1]:
-                child.resize(*child_size)
+                child.resize(child_size)
+            self.children[-1].resize(last_child_size)
 
     def action(self, event):
         if self.event_handler:
@@ -197,7 +320,7 @@ class Element:
                     else:
                         children_sum += 1
                 children_sum += child.cur_size.x
-            return child
+            return self.children[-1]
 
     def element_at(self, x, y):
         """
@@ -215,6 +338,9 @@ class Element:
         else:
             return self
 
+    def with_id(self, id_str: str):
+        self.id = id_str
+        return self
 
     def element_at_old(self, x, y):
         """
@@ -241,7 +367,7 @@ class Element:
                         start_of_child = row_after_child + 1
                 else:
                     start_of_child = row_after_child
-            return child.element_at(x, y - start_of_child + child.cur_size.y)
+            return self.children[-1].element_at(x, y - start_of_child + self.children[-1].cur_size.y)
             # children_sum = 0
             # for child in self.children:
             #     if children_sum + child.cur_size.y >= y + 1:
@@ -254,11 +380,11 @@ class Element:
             for child in self.children:
                 if children_sum + child.cur_size.x >= x + 1:
                     # end of child is after x
-                    break
+                    return child.element_at(x - (children_sum - child.cur_size.x), y)
                 children_sum += child.cur_size.x
                 if self.separate:
                     children_sum += 1
-            return child.element_at(x - (children_sum - child.cur_size.x), y)
+            return self.children[-1].element_at(x - (children_sum - self.children[-1].cur_size.x), y)
 
     def pos_of_child(self, child):
         """
@@ -284,10 +410,23 @@ class Element:
     def with_child(self, child):
         self.children.append(child)
         self.min_size += child.min_size
+        child.parent = self
         return self
 
-    def with_content(self, content):
-        self.content = content
+    def with_content(self, content, update: bool = False):
+        """
+        Sets the content of this element.
+        :param content: Either the content itself or a function or other
+        callable that takes no arguments and returns the content
+        :param update: If True, self.update() will be called after setting the content
+        :return: This element
+        """
+        if callable(content):
+            self.fetch_content = content
+        else:
+            self.fetch_content = lambda _: content
+        if update:
+            self.update()
         return self
 
     def with_direction(self, direction):
@@ -327,65 +466,21 @@ class Element:
                 joiner = self.separator() if self.separate else ""
                 return joiner.join([child.get_content(row) for child in self.children])
 
-        content_str = self.content()
-        if content_str:
-            start = row*self.cur_size.x
-            end = (row+1)*self.cur_size.x
-            if isinstance(content_str, list):
-                content_str = "\n".join([str(c) for c in content_str])
-            flown = flow_text(content_str, self.cur_size.x)
-            return flown[row] if row < len(flown) else self.empty_row()
-        return "?" * self.cur_size.x
+        if not self.content:
+            return "?" * self.cur_size.x
+
+        if isinstance(self.content, list):
+            content_str = "\n".join([str(c) for c in self.content])
+        else:
+            content_str = str(self.content)
+        flown = flow_text(content_str, self.cur_size.x)
+        return flown[row] if row < len(flown) else self.empty_row()
 
     def empty_row(self):
         return " " * self.cur_size.x
 
     def with_border(self, border_char='#'):
         return Border(self, border_char)
-
-
-class BorderedElement(Element):
-    def __init__(self, border_char='#'):
-        super(BorderedElement, self).__init__()
-        self.border = border_char
-
-    def resize(self, w, h):
-        self.cur_size = Point(w, h)
-        w -= 2
-        h -= 2
-        if len(self.children) > 0:
-            if self.direction == 'vertical':
-                child_size = Point(w, h // len(self.children))
-            else:
-                child_size = Point(w // len(self.children), h)
-            for child in self.children:
-                child.resize(*child_size)
-
-    def get_content(self, row):
-        if row == 0 or row == self.cur_size.y - 1:
-            # first and last rows are border
-            return self.border * self.cur_size.x
-        row -= 1
-        if len(self.children) != 0:
-            if self.direction == 'vertical':
-                # find which child must write row
-                childern_sum = 0
-                for child in self.children:
-                    if childern_sum + child.cur_size.y >= row + 1:
-                        # end of child is after current row
-                        break
-                    childern_sum += child.cur_size.y
-                return self.border + child.get_content(row - childern_sum) + self.border
-            else:
-                s = self.border
-                for child in self.children:
-                    s += child.get_content(row)
-                return s + self.border
-        if self.content:
-            start = row*(self.cur_size.x-2)
-            end = (row+1)*(self.cur_size.x-2)
-            return self.border + str(self.content)[start:end].ljust(self.cur_size.x - 2) + self.border
-        return self.border + ("?" * (self.cur_size.x - 2)) + self.border
 
 
 class Border(Element):
@@ -402,6 +497,9 @@ class Border(Element):
     def add_child(self, child):
         self.children[0].add_child(child)
 
+    def set_child(self, child):
+        self.children[0] = child
+
     def resize(self, w, h):
         self.cur_size = Point(w, h)
         self.children[0].resize(w-2, h-2)
@@ -414,24 +512,46 @@ class Border(Element):
             return self.border + self.children[0].get_content(row - 1) + self.border
 
 
-class Event:
-    pos = None
-    key = None
+@unique
+class EventKind(Enum):
+    UNKNOWN = auto()
+    USER_INPUT = auto()
+
+    def _generate_next_value_(name, start, count, last_values):
+        return name
 
 
-def inc(state):
-    state.properties['counter'] += 1
+class Event():
+    def __init__(self, kind: EventKind = EventKind.UNKNOWN):
+        self.kind = kind
+        self.pos = None
+        self.key = None
+
+    def at_position(self, x, y):
+        self.pos = Point(x, y)
 
 
-def debug_info():
-    return ['absolute cursor position: {}'.format(state.pos),
+class Debug:
+    recent_inputs = DroppingList(10)
+
+
+def debug_info(controller, elem):
+    root = controller.roots[0]
+    state = controller.state
+    if 'frame' not in state.properties:
+        state.properties['frame'] = 0
+    return ['this element: {}'.format(elem),
+            'absolute cursor position: {}'.format(state.pos),
             'elem under cursor: {}'.format(root.element_at(*state.pos)),
             'first child under cursor: {}'.format(root.child_at(*state.pos)),
             'frame: {}'.format(state.properties['frame']),
-            'debug: {}'.format(state.debug)]
+            'debug: {}'.format(Debug.recent_inputs)]
 
 
-def getch_timeout(timeout_millis: int = 500) -> bytes:
+def getch(timeout_millis: int = 0) -> bytes:
+    if timeout_millis <= 0:
+        return msvcrt.getch()
+
     start_time = time.perf_counter()
     while not (msvcrt.kbhit() or (time.perf_counter() - start_time) * 1000 > timeout_millis):
         time.sleep(0.001)
@@ -445,57 +565,66 @@ class TerminationRequestedException(Exception):
     pass
 
 
-def process_user_input(state, user_input):
-    state.debug.append(user_input)
+class Foo:
+    def __init__(self):
+        self.bar_fn = lambda self: print(self)
 
-    if user_input == b'\x03':
-        raise KeyboardInterrupt
-    if user_input in [b'\000', b'\xe0']:
-        pass
+    def foo(self):
+        print(self)
 
-    if user_input in [b'r', b'M']:
-        state.pos.x = min(terminal_size.x - 1, state.pos.x + 1)
-    elif user_input in [b'l', b'K']:
-        state.pos.x = max(0, state.pos.x - 1)
-    elif user_input in [b'd', b'P']:
-        state.pos.y = min(terminal_size.y - 1, state.pos.y + 1)
-    elif user_input in [b'u', b'H']:
-        state.pos.y = max(0, state.pos.y - 1)
-    elif user_input == b'\r':
-        event = Event()
-        event.pos = state.pos
-        event.key = user_input
-        root.action(event)
-    elif user_input == b'\x1b':
-        raise TerminationRequestedException
-    # else:
-    #     print("exiting. input was: " + str(user_input))
-    #     break
+    @property
+    def bar(self, *args):
+        return self.bar_fn(self, *args)
+
+    @bar.setter
+    def bar(self, bar):
+        self.bar_fn = bar
+
+
+def main():
+    input("Press the return key to continue...")
+    counter = Box(0)
+    # counter = [0]
+
+    def inc():
+        counter.__iadd__(1)
+
+    controller = Controller()
+    root = (Element()
+            .with_id('.')
+            .with_direction('horizontal')
+            .with_handler(lambda elem, event: inc())
+            .with_child(Element()
+                        .with_id('.0')
+                        .with_content(lambda elem: debug_info(controller, elem))
+                        )
+            .with_child(Element()
+                        .with_id('.1')
+                        .with_child(Element()
+                                    .with_id('.1.0')
+                                    .with_content(controller.state.pos))
+                        .with_child(Element()
+                                    .with_id('.1.1')
+                                    .with_content(lambda _: str(root.pos_of_child(1))))
+                        .with_child(Element()
+                                    .with_id('.1.2:Counter')
+                                    .with_content(lambda _: "counter: {}".format(counter))
+                                    .with_handler(lambda elem, event: inc())
+                                    )
+                        )
+            )
+    controller.attach(root)
+
+    while True:
+        controller.update()
+        draw(root, controller.state)
+        user_input = getch(AUTO_REFRESH_INTERVAL)
+        if user_input:
+            try:
+                controller.process_user_input(user_input)
+            except TerminationRequestedException:
+                break
 
 
 if __name__ == '__main__':
-    # input("Press the return key to continue...")
-    state = State()
-    state.debug = DroppingList(10)
-    state.properties['counter'] = 0
-
-    root = (Element()
-            .with_direction('horizontal')
-            .with_child(Element().with_content(debug_info))
-            .with_child(Element()
-                        .with_child(Element().with_content(lambda: str(state.pos)))
-                        .with_child(Element().with_content(lambda: str(root.pos_of_child(1))))
-                        .with_child(Element()
-                                    .with_content(lambda: "counter: {}".format(state.properties['counter']))
-                                    .with_handler(lambda elem, event: inc(state)))
-                        )
-            )
-
-    while True:
-        draw(root, state)
-        user_input = getch_timeout()
-        if user_input:
-            try:
-                process_user_input(state, user_input)
-            except TerminationRequestedException:
-                break
+    main()
